2e544ecdbe62567637a3ec17c076c5bc9bc15d77
vatsalrami <vatsalrami@gmail.com>
2024-09-03 14:14:02 -0400

    remove test

--- backend/src/amazonscraper.js
+++ backend/src/amazonscraper.js
@@ -39,9 +39,3 @@ async function scrapeAmazonProduct(url) {
 
   return product;
 }
-
-const url =
-  "https://www.amazon.com/Adhesive-Upgraded-Management-Christmas-Ethernet/dp/B08ZJF9W33/ref=sr_1_8?crid=2CA7ABRPQ6EYY&dib=eyJ2IjoiMSJ9.npjnPy6Ci1pOcQh7mmKq4PwJkm-7uNFJRmPHIW27qLgR4ecL9D3XgTanZFyNM5yg90JL-xBvtV2qapeX-xOWM2SZJQRPkXzapIBFTO0sd_Kl6fvMDLlM0bYIjDnmoo49LM94bxP3gsYKY-wcKdk8oQfU5lP4k_YuCU6gS4Yz5BYD0YII6la-hpUzmkcnMjdMUW0nLwAgfI_h344M7su6k_nman6MGYOAR5hvKAETJDM.gpQdjYdPsDMuEO3Gpq7xo6q5IfB_qBAJwCgU7_rdkoE&dib_tag=se&keywords=cable%2Bmanagement&qid=1725385294&sprefix=cable%2B%2Caps%2C75&sr=8-8&th=1";
-scrapeAmazonProduct(url)
-  .then((product) => console.log(product))
-  .catch((error) => console.error("Error scraping Amazon product:", error));
